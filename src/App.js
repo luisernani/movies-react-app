aecda32a6e137df2e1a6bf75892a99dded4a8c86
Luis Ernani <luisernani87@gmail.com>
2023-11-11 22:17:58 -0300

    Starting Movie App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import React, { useEffect } from "react";
 import './App.css';
+import SearchIcon from './search.svg';
+
+//====================================
+// Api key from omdbapi.com = 3bce88a4
+//====================================
+const API_URL = 'http://www.omdbapi.com?apikey=3bce88a4';
 
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-  const [counter, setCounter] = useState(0);
+        console.log(data.Search);
+    }
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
 
-  return (
-    <div className="App">
-      <h1>Hello World!! This is gamarei</h1>
-      <h1>Counter</h1>
-      <button onClick={() => setCounter((prevCounter) => prevCounter - 1 )}>-</button>
-      <h2>{counter}</h2>
-      <button onClick={() => setCounter((prevCounter) => prevCounter + 1 )}>+</button>
-
-    </div>
-  );
-}
+    return(
+        <div className="app">
+            <h1>Gamarei Moviessss</h1>
+        </div>
+    );
+};
 
 export default App;
