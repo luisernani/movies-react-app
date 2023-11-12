9319e7138bcdc3b9a7d71dc32474c0df7abfd2c2
Luis Ernani <luisernani87@gmail.com>
2023-11-11 21:01:31 -0300

    Counter Working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <h1>Hello World!! This is gamarei</h1>
+      <h1>Counter</h1>
+      <button onClick={() => setCounter((prevCounter) => prevCounter - 1 )}>-</button>
+      <h2>{counter}</h2>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 1 )}>+</button>
+
     </div>
   );
 }
